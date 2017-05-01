8834579c2435b869daadd54dc654cd6235679ed6
rb0three <rizenback000@gmail.com>
2017-05-01 15:35:34 +0900

    DOM操作変数の名前変更と説明文章の定数化

--- NicotagLinker.user.js
+++ NicotagLinker.user.js
@@ -1,32 +1,35 @@
 // ==UserScript==
-// @name           nicotag linker
+// @name           Nicotag Linker
 // @namespace      http://unote.hatenablog.com/
 // @description    ニコ動のタグに設定された動画IDをリンクとして抽出する
 // @include        http://www.nicovideo.jp/watch/*
 // @grant none
 // ==/UserScript==
 
-(function(){
-	//Flash版
-	var vheader = document.getElementById("videoMenuWrapper");
+(function() {
+  const VERSION = "1.0.0";
+  const JUMP_TEXT = "[動画へのリンク] ";
+  
+  //Flash版
+  var vheader = document.getElementById("videoMenuWrapper");
   var tags = document.getElementsByClassName("videoHeaderTagLink");
-	//HTML5版
-	if (vheader == null){
-		vheader = document.getElementsByClassName("MainContainer")[0];
-		tags = document.getElementsByClassName("TagItem-name");
-	}
+  //HTML5版
+  if (vheader == null) {
+    vheader = document.getElementsByClassName("MainContainer")[0];
+    tags = document.getElementsByClassName("TagItem-name");
+  }
 
-	var jump_div = document.createElement("div");
-	jump_div.textContent = "[動画タグへのリンク] ";
-	
-	for(var i=0; i < tags.length; i++) {
-		if( /([sn]m\d+)/.test(tags[i].textContent) ){
-			var jump_link = document.createElement("a");
-			jump_div.appendChild(jump_link);
-			jump_link.text = tags[i].textContent;
-			jump_link.href = "watch/" + RegExp.$1;
-			jump_link.style.marginRight = "1em";
-		}
-	}
-	vheader.parentNode.insertBefore(jump_div, vheader);
+  var jumpDiv = document.createElement("div");
+  jumpDiv.textContent = JUMP_TEXT;
+
+  for (var i = 0; i < tags.length; i++) {
+    if (/([sn]m\d+)/.test(tags[i].textContent)) {
+      var jumpLink = document.createElement("a");
+      jumpDiv.appendChild(jumpLink);
+      jumpLink.text = tags[i].textContent;
+      jumpLink.href = "watch/" + RegExp.$1;
+      jumpLink.style.marginRight = "1em";
+    }
+  }
+  vheader.parentNode.insertBefore(jumpDiv, vheader);
 })();
